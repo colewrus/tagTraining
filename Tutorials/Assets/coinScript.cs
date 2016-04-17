7d363515d42ef94b307a1193f65cb3e29a3e9d31
colewrus <wrampelc@gmail.com>
2016-04-16 22:57:41 -0400

    first commit

---
+++ Tutorials/Assets/coinScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class coinScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			Debug.Log (col.gameObject.tag);
+			addScore ();
+			Destroy (this.gameObject, 0.2f);
+		}
+	}
+
+	void addScore(){
+		playerDeath sn = gameObject.GetComponent<playerDeath> ();
+		sn.scoreInc;
+
+	}
+}
