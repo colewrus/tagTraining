2f6c252df6c49b3d26eb44e5d0be719a00c3c923
colewrus <wrampelc@gmail.com>
2016-04-18 21:44:00 -0400

    workshop complete with coin addition and life system

--- Tutorials/Assets/coinScript.cs
+++ Tutorials/Assets/coinScript.cs
@@ -1,32 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class coinScript : MonoBehaviour {
 
+	public GameObject spawnSpot;
+	public Text coinText;
+	public Text lifeText;
+	int coinScore;
+	int lives;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		coinScore = 0;
+		lives = 3;
+		lifeText.text = "Lives: " + lives;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (this.transform.position.y < -10.0f) {
+			this.transform.position = spawnSpot.transform.position;
+			lives -= 1;
+			lifeText.text = "Lives: " + lives;
+		}
 
-	}
-
-	void OnTriggerEnter(Collider col){
-		if (col.gameObject.tag == "Player") {
-			Debug.Log (col.gameObject.tag);
-			addScore ();
-			Destroy (this.gameObject, 0.2f);
+		if (lives == 0) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 	}
 
-	void addScore(){
-		playerDeath sn = gameObject.GetComponent<playerDeath> ();
-		sn.scoreInc;
 
+	void OnTriggerEnter(Collider col){
+		coinScore += 1;
+		coinText.text = "Score: " + coinScore;
+		Destroy (col.gameObject);
 	}
+
 }
